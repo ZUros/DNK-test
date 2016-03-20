113aabe8c748cd2378f00a7a14bf1a36f54b0c21
Uros <ZUros>
2016-03-21 00:46:54 +0100

    koncan_program

--- DNK_filter.py
+++ DNK_filter.py
@@ -1,24 +1,50 @@
-spremenljivka = open ("DNK.txt").read()
+dokaz = open ("DNK.txt").read()
 
-print "Dolzina DNK vijacnice:" + str(len(spremenljivka))
+print "Dolzina DNK vijacnice:" + str(len(dokaz))
 
-print "Barva las crna:", spremenljivka.find("CCAGCAATCGC")
-print "Barva las rjava:", spremenljivka.find("GCCAGTGCCG")
-print "Barva las korencek:", spremenljivka.find("TTAGCTATCGC")
+#hitro iskanje
+print "Barva las crna:", dokaz.find("CCAGCAATCGC")
+print "Barva las rjava:", dokaz.find("GCCAGTGCCG")
+print "Barva las korencek:", dokaz.find("TTAGCTATCGC")
 
-print "Kvadratna oblika obraza:", spremenljivka.find("GCCACGG")
-print "Okrogla oblika obraza:", spremenljivka.find("ACCACAA")
-print "Ovalna oblika obraza:", spremenljivka.find("AGGCCTCA")
+print "Kvadratna oblika obraza:",dokaz.find("GCCACGG")
+print "Okrogla oblika obraza:", dokaz.find("ACCACAA")
+print "Ovalna oblika obraza:", dokaz.find("AGGCCTCA")
 
-print "Modra barva oci:", spremenljivka.find("TTGTGGTGGC")
-print "Zelena barva oci:", spremenljivka.find("GGGAGGTGGC")
-print "Rjava barva oci:", spremenljivka.find("AAGTAGTGAC")
+print "Modra barva oci:", dokaz.find("TTGTGGTGGC")
+print "Zelena barva oci:", dokaz.find("GGGAGGTGGC")
+print "Rjava barva oci:", dokaz.find("AAGTAGTGAC")
 
-print "Moski spol:", spremenljivka.find("TGCAGGAACTTC")
-print "Zenski spol:", spremenljivka.find("TGAAGGACCTTC")
+print "Moski spol:", dokaz.find("TGCAGGAACTTC")
+print "Zenski spol:", dokaz.find("TGAAGGACCTTC")
 
-print "Belec:", spremenljivka.find("AAAACCTCA")
-print "Crnec:", spremenljivka.find("CGACTACAG")
-print "Azijec:", spremenljivka.find("CGCGGGCCG")
+print "Belec:", dokaz.find("AAAACCTCA")
+print "Crnec:", dokaz.find("CGACTACAG")
+print "Azijec:", dokaz.find("CGCGGGCCG")
+
+#Parametri:
+Barva_las_crna="CCAGCAATCGC"
+Barva_las_rjava="GCCAGTGCCG"
+Barva_las_korencek="TTAGCTATCGC"
+Kvadratna_oblika_obraza="GCCACGG"
+Okrogla_oblika_obraza="ACCACAA"
+Ovalna_oblika_obraza="AGGCCTCA"
+Zelena_barva_oci="GGGAGGTGGC"
+Modra_barva_oci="TTGTGGTGGC"
+Rjava_barva_oci="AAGTAGTGAC"
+Moski_spol="TGCAGGAACTTC"
+Zenski_spol="TGAAGGACCTTC"
+Belec="AAAACCTCA"
+Crnec="CGACTACAG"
+Azijec="CGCGGGCCG"
+
+
+#Izpis imena:
+if dokaz.find(Barva_las_korencek) and dokaz.find(Okrogla_oblika_obraza) and dokaz.find(Rjava_barva_oci) > 0:
+    print "Sladoled je pojedel Ziga!"
+elif dokaz.find(Barva_las_crna) and dokaz.find(Ovalna_oblika_obraza) and dokaz.find(Modra_barva_oci) > 0:
+    print "Sladoled je pojedel Matej!"
+elif dokaz.find(Barva_las_rjava) and dokaz.find(Kvadratna_oblika_obraza) and dokaz.find(Zelena_barva_oci) > 0:
+    print "Sladoled je pojedel Miha!"
 
 
